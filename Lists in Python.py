be31f8d2ad187147050beb27d5bfd7e5d684dfeb
falconmark95 <59381727+falconmark95@users.noreply.github.com>
2021-11-05 17:37:41 -0500

    note update 5:37 11.5.21

--- Lists in Python.py
+++ Lists in Python.py
@@ -2,5 +2,11 @@
 my_list = ['one','two','three',4,5]
 v1 = my_list[1]
 v2 = len(v1)
+
+# Printing the range of items to display from the list
 print(my_list[2:4])
+
+# Getting the length of the item in the list
 print(len(my_list[2:4]))
+
+# Concatenating strings
