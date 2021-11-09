b3dc3e095826c085dd0b8b10b5ad8e9703e43b47
falconmark95 <59381727+falconmark95@users.noreply.github.com>
2021-11-09 14:23:37 -0600

    Update Comparison Operators.py

--- Python Comparison Operators/Comparison Operators.py
+++ Python Comparison Operators/Comparison Operators.py
@@ -1,1 +1,45 @@
-asdasd
+# ~Comparison Operators~
+
+"""
+==	If the values of two operands are equal, then the condition becomes true.
+    (a == b) is not true.
+
+!=	If values of two operands are not equal, then condition becomes true.
+    (a != b) is true
+
+>	If the value of left operand is greater than the value of right operand, then condition becomes true.
+    (a > b) is not true.
+
+<	If the value of left operand is less than the value of right operand, then condition becomes true.
+    (a < b) is true.
+
+>=	If the value of left operand is greater than or equal to the value of right operand, then condition becomes true.
+    (a >= b) is not true.
+
+<=	If the value of left operand is less than or equal to the value of right operand, then condition becomes true.
+    (a <= b) is true.
+"""
+
+# Equal
+print(2 == 2)
+print(1 == 0)
+
+# Not Equal
+print(2 != 1)
+print(2 != 2)
+
+# Greater than
+print(2 > 1)
+print(2 > 4)
+
+# Less than
+print(2 < 4)
+print(2 < 1)
+
+# Greater than or Equal to
+print(2 >= 2)
+print(2 >= 1)
+
+# Less than or Equal to
+print(2 <= 2)
+print(2 <= 4)
